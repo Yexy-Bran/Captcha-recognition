48734ccc6ed0a1913d43fa97251b0ee4f8958ba9
Yexy-Bran <37256778+Yexy-Bran@users.noreply.github.com>
2018-03-11 03:21:58 +0000

    Update train_lenet_adam.sh

--- Captcha/train_lenet_adam.sh
+++ Captcha/train_lenet_adam.sh
@@ -3,4 +3,4 @@ set -e
 
 ./build/tools/caffe train \
     --solver=examples/Captcha/lenet_solver_adam.prototxt \
-    --snapshot=examples/Captcha/lenet_iter_4182.solverstate $@
+    #--snapshot=examples/Captcha/lenet_iter_4182.solverstate $@
